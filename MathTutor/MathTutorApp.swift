b7f24916d535c23cf1e2c59564191e166948551b
Miguel <mgamez20@icloud.com>
2025-06-15 15:59:27 -0400

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Miguel on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathTutorTests/MathTutorTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorTests.swift
+//  MathTutorTests
+//
+//  Created by Miguel on 6/15/25.
+//
+
+import Testing
+@testable import MathTutor
+
+struct MathTutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
